7d538cfa659d38f24d321d20c9e6102df9550811
Benyouceftebbi <benyoucef.tebbi@std.antalya.edu.tr>
2025-07-02 23:32:07 +0100

    new

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,14 +4,13 @@ import {collection, getDocs, getFirestore, query, where} from 'firebase/firestor
 import { getStorage } from "firebase/storage";
 import { connectFunctionsEmulator, getFunctions } from 'firebase/functions';
 const firebaseConfig = {
-    apiKey: "AIzaSyDf8fWEqcsW4MW5xUj-jXIUiSRKib06GpQ",
-    authDomain: "tullin-ecce0.firebaseapp.com",
-    projectId: "tullin-ecce0",
-    storageBucket: "tullin-ecce0.firebasestorage.app",
-    messagingSenderId: "731579369642",
-    appId: "1:731579369642:web:cb2267044a6f3d34e6e8d3"
-  };
-
+  apiKey: "AIzaSyBfoLma--moBUmiBp4kpxXJ6krLrwcduPU",
+  authDomain: "khaled-37140.firebaseapp.com",
+  projectId: "khaled-37140",
+  storageBucket: "khaled-37140.firebasestorage.app",
+  messagingSenderId: "893789448214",
+  appId: "1:893789448214:web:2349e223ed8c2c03436d37"
+};
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
